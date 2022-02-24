85e55e55c7257a7b7d91998ac2bbf3f86f826c06
Paulo Preto <pauloa_preto@hotmail.com>
2022-02-24 20:44:42 +0000

    initiate project

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/!(*.stories).{ts,tsx}'],
+  modulePaths: ['<rootDir>/src/'],
+};
