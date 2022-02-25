5298b462a07adfa95223aaf2e7217d42315f5a64
Paulo Preto <pauloa_preto@hotmail.com>
2022-02-25 19:41:19 +0000

    format code with lint rules

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -32,10 +32,11 @@ export const Button = ({ primary = false, size = 'medium', backgroundColor, labe
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
-      type="button"
+      type='button'
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={{ backgroundColor }}
-      {...props}>
+      {...props}
+    >
       {label}
     </button>
   );
