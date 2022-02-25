151aed278ab7bd450cf1d5fbdff2b695bd1daed4
Paulo Preto <pauloa_preto@hotmail.com>
2022-02-25 23:21:53 +0000

    add custom styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#202224',
+        light: '#E5E5E5',
+        beje: '#B9AEA0',
+      },
+      boxShadow: {
+        keyUp: '6px 6px 12px #1C1E1F, -6px -6px 12px #242629',
+        keyDown: 'inset 6px 6px 12px #1C1E1F, inset -6px -6px 12px #242629',
+      },
+      width: {
+        keyboard: '70.75rem',
+      },
+      height: {
+        keyboard: '24.5rem',
+      },
+    },
   },
   plugins: [],
 };
