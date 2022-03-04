1072652863cf91869f1251bae14d1e057e80abc9
paulopreto <paulopreto.celfocus@gmail.com>
2022-03-04 21:56:14 +0000

    add svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,18 @@ import react from '@vitejs/plugin-react';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh(), tsconfigPaths()],
+  plugins: [
+    react(),
+    reactRefresh(),
+    tsconfigPaths(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
