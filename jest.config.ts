65512e81901af87888491d860b7489c11f0f0da9
Paulo Preto <pauloa_preto@hotmail.com>
2022-04-05 21:35:41 +0100

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,15 @@ export default {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
   },
   moduleNameMapper: {
-    '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|mp3)$': 'jest-transform-stub',
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub',
+    '\\.svg': '<rootDir>/__mocks__/svg.js',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/!(*.stories).{ts,tsx}'],
+  collectCoverageFrom: ['src/**/!(*.stories).{ts,tsx}', '!<rootDir>/src/main.tsx'],
   modulePaths: ['<rootDir>/src/'],
 };
